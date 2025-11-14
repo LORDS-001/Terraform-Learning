872717a050ed5246f613667272a447166814c0f5
Daniel Adedokun <adedokundaniel16@gmail.com>
2025-11-14 02:39:59 +0100

    Multiple Instance

--- Terraform/Start With Terraform Basics/createinstance.tf
+++ Terraform/Start With Terraform Basics/createinstance.tf
@@ -1,4 +1,9 @@
 resource "aws_instance" "MyFirstInstance" {
+    count         = 3
     ami           = "ami-000e50175c5f86214"
-    instance_type = "t2.micro"
+    instance_type = "t3.micro"
+
+    tags = {
+        Name = "threedemoinstance"
+    }
 }
